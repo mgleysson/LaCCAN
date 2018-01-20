621739b72fb320cee79d3c9875c2f00dc17dee82
Marcos Gleysson <marcos.gleysson@gmail.com>
2018-01-20 00:12:35 -0300

    refazendo os gráficos utilizando ggplot2

--- Code/R/Scripts Iniciais/DensGI0.R
+++ Code/R/Scripts Iniciais/DensGI0.R
@@ -6,23 +6,31 @@ densGI0 <- function(z, alpha, gamma, Looks) {
     (z^(Looks-1) / (gamma + Looks * z)^(Looks-alpha)))
 }
 
-densGI0(0.07996402, -2, 4, 7)
+d1 <- densGI0(0.07996402, -2, 4, 7)
 
-densGI0(0.07996402, -2, 4, 8)
+d2 <- densGI0(0.07996402, -2, 4, 8)
 
-densGI0(0.07996402, -2, 4, 9)
+d3 <- densGI0(0.07996402, -2, 4, 9)
 
-densGI0(0.07996402, -2, 1, 9)
+d4 <- densGI0(0.07996402, -2, 1, 9)
 
-densGI0(0.07996402, -2, 2, 9)
+d5 <- densGI0(0.07996402, -2, 2, 9)
 
-densGI0(0.07996402, -2, 3, 9)
+d6 <- densGI0(0.07996402, -2, 3, 9)
 
-densGI0(0.07996402, -3, 3, 9)
+d7 <- densGI0(0.07996402, -3, 3, 9)
 
-densGI0(0.07996402, -2, 3, 9)
+d8 <- densGI0(0.07996402, -2, 3, 9)
 
-densGI0(0.07996402, -1, 3, 9)
+d9 <- densGI0(0.07996402, -1, 3, 9)
+
+dn <- c(d1, d2, d3, d4, d5, d6, d7, d8, d9) 
+
+densityGIO <- data.frame(y = dn)
+
+ggplot(densityGIO, aes(y)) + geom_density(adjust = 1) +
+  xlab("Densidades") + ylab("Frequencia")
+
 
 
 
--- Code/R/Scripts Iniciais/LCG.R
+++ Code/R/Scripts Iniciais/LCG.R
@@ -1,5 +1,7 @@
 # Analisando o funcionamento dos Geradores Lineares Congruenciais
 
+library(ggplot2)
+
 lcg <- function(M, a, c, seed, size) {
   r = numeric(size)
   r[1] = seed
@@ -14,8 +16,29 @@ seq1 <- lcg(M = 16, a = 5, c = 1, seed = 5, size = 34)
 seq2 <- lcg(M = 86436, a = 1093, c = 18257, seed = 7, size = 100)
 seq3 <- lcg(M = 394, a = 34, c = -102, seed = 404, size = 50)
 
+dados1 <- data.frame(y = seq1)
 
-hist(seq1, breaks = 10)
-hist(seq2, breaks = 15)
-hist(seq3,breaks = 20)
+h1 <- ggplot(dados1, aes(y)) + geom_histogram(binwidth = 0.08) +
+xlab("Dados da seq 1") + ylab("Frequencia")
+
+h1
+
+dados2 <- data.frame(y = seq2)
+
+h2 <- ggplot(dados2, aes(y)) + geom_histogram(binwidth = 0.08) +
+  xlab("Dados da seq 2") + ylab("Frequencia")
+
+h2
+
+dados3 <- data.frame(y = seq3)
+
+h3 <- ggplot(dados3, aes(y)) + geom_histogram(binwidth = 0.08) +
+  xlab("Dados da seq 3") + ylab("Frequencia")
+
+h3
+
+
+#hist(seq1, breaks = 10)
+#hist(seq2, breaks = 15)
+#hist(seq3,breaks = 20)
 
